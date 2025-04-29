098784636891eaad9ecaf045ec2211da69c6ce46
Iurii Anufriev <aiuanufriev@asuproject.ru>
2025-04-29 21:05:47 +0500

    ag-grid-2

---
+++ client/src/components/table/TableDataSource.tsx
@@ -0,0 +1,29 @@
+import React from "react"
+import {IServerSideDatasource, IServerSideGetRowsParams} from 'ag-grid-enterprise'
+import axios, {AxiosResponse} from "axios";
+
+export interface IGridData {
+    id: number
+    value: string
+}
+
+const TableDataSource = (): IServerSideDatasource<IGridData> => {
+    const createServerSideDatasource = (): IServerSideDatasource<IGridData> => {
+        return {
+            getRows: async (params: IServerSideGetRowsParams<IGridData>) => {
+                const response: AxiosResponse = await axios.get('/api/grid', {
+                    params: {
+                        startRow: params.request.startRow,
+                        endRow: params.request.endRow
+                    }
+                })
+                const result: IGridData[] = await response.data
+                params.success({rowData: result})
+            }
+        }
+    }
+
+    return createServerSideDatasource()
+}
+
+export default TableDataSource
