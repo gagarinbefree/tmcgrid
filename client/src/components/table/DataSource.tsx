c6ac027fb025f6b3e7fded5cc2d66c4157098177
Iurii Anufriev <aiuanufriev@asuproject.ru>
2025-05-06 16:25:20 +0500

    search, sort, select

---
+++ client/src/components/table/DataSource.tsx
@@ -0,0 +1,63 @@
+import {IServerSideDatasource, IServerSideGetRowsParams} from 'ag-grid-enterprise'
+import axios, {AxiosError, AxiosResponse} from "axios"
+
+export interface IGridData {
+    id: string
+    value: string
+    checked: boolean
+}
+
+export interface ITableDataSource {
+    createServerSideDatasource: (search: string) => IServerSideDatasource<IGridData>
+    updateRowOrderOnServer: (rowId: number, newIndex: number) => Promise<void>
+    updateCheckedOnServer: (id: number) => Promise<void>
+}
+
+const DataSource = (): ITableDataSource => {
+    const source: ITableDataSource = {
+        createServerSideDatasource(search: string): IServerSideDatasource<IGridData> {
+            return {
+                getRows: async (params: IServerSideGetRowsParams<IGridData>) => {
+                    try {
+
+                        const response: AxiosResponse = await axios.get('/api/grid', {
+                            params: {
+                                start: params.request.startRow,
+                                end: params.request.endRow,
+                                search
+                            }
+                        })
+                        const result: IGridData[] = await response.data
+                        params.success({rowData: result})
+                    }
+                    catch (e: any) {
+                        params.success({rowData: []})
+                        console.error(e?.message)
+                    }
+                }
+            }
+        },
+
+        async updateRowOrderOnServer(id: number, index: number): Promise<void> {
+            try {
+                await axios.post('/api/grid/order', { id, index })
+            }
+            catch (e: any) {
+                console.error(e?.message)
+            }
+        },
+
+        async updateCheckedOnServer(id: number): Promise<void> {
+            try {
+                await axios.post('/api/grid/checked', {id})
+            }
+            catch (e: any) {
+                console.error(e?.message)
+            }
+        }
+    }
+
+    return source
+}
+
+export default DataSource()
