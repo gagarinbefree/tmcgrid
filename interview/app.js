522deaa82d5e8849d1690a8041ea61e356b91dc2
Iurii Anufriev <aiuanufriev@asuproject.ru>
2025-06-10 12:46:16 +0300

    parallel exec

--- interview/app.js
+++ interview/app.js
@@ -1,19 +1,32 @@
 // Тест на глубину понимания работы интерпретатора
+
 async function delay(ms) {
     return new Promise(r=>setTimeout(r,ms))
 }
 async function short(){
     await delay(500)
-    throw new Error("short() thrown");
+    throw new Error("short() throwed");
 }
+
 async function long(){
     await delay(1000)
     return 'long() result';
 }
 async function start() {
     try{
-        console.log(await long());
-        console.log(await short());
+        let promise1 = short().catch(e => console.error(e.message))
+        let promise2 = long().catch(e => console.error(e.message))
+
+        const longResult = await promise2;
+        if (!longResult)
+            return
+        console.log(longResult); // 1000ms timeout
+
+        const shortResult  = await promise1;
+        if (!shortResult)
+            return
+        console.log(shortResult); // 500ms timeout
+
         console.log('success');
     }
     catch(e){
@@ -21,13 +34,8 @@ async function start() {
     }
 }
 
-(async () => {
-    await start();
-    console.log('test done');
-})();
+start().then(()=>console.log('test done'));
 
-// yarn run start
-// выходные данные
-// long() result
-// CATCH ERROR
-// test done
+// описать выходные данные, пример:
+// 1 - CATCH ERROR
+// 2 - test done
