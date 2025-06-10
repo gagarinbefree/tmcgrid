e5854b2b0fde6f70600937b14731b6cb1b6b7fad
Iurii Anufriev <aiuanufriev@asuproject.ru>
2025-06-10 10:41:02 +0300

    interview code fix

---
+++ interview/src/app.js
@@ -0,0 +1,41 @@
+// Тест на глубину понимания работы интерпретатора
+
+async function delay(ms) {
+    return new Promise(r=>setTimeout(r,ms))
+}
+async function short(){
+    await delay(500)
+    throw new Error("short() throwed");
+}
+
+async function long(){
+    await delay(1000)
+    return 'long() result';
+}
+async function start() {
+    try{
+        console.log( await long());
+        console.log(await short());
+        console.log('success');
+
+        /*let promise1 = short();
+        let promise2 = long();
+        console.log(await promise2); // 1000ms timeout
+        console.log(await promise1); // 500ms timeout
+        console.log('success');*/
+    }
+    catch(e){
+        console.error("CATCH ERROR");
+    }
+}
+
+(async () => {
+    await start();
+    console.log('test done');
+})()
+
+//start().then(()=>console.log('test done'));
+
+// описать выходные данные, пример:
+// 1 - CATCH ERROR
+// 2 - test done
